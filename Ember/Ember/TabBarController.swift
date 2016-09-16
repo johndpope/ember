fa35b4019b1dd8a30b40f19ab793c2e635779b73
Anthony Maina <anthony.maina@savana.co.ke>
2016-09-15 20:31:36 -0500

    Change TabBar items blue tint

--- Ember/Ember/TabBarController.swift
+++ Ember/Ember/TabBarController.swift
@@ -12,32 +12,7 @@ class TabBarController: UITabBarController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-//        // set red as selected background color
-//        let numberOfItems = CGFloat(tabBar.items!.count)
-//        let tabBarItemSize = CGSize(width: tabBar.frame.width / numberOfItems, height: tabBar.frame.height)
-//        tabBar.selectionIndicatorImage = UIImage.imageWithColor(UIColor.darkGrayColor(), size: tabBarItemSize).resizableImageWithCapInsets(UIEdgeInsetsZero)
-//        
-//        // remove default border
-//        tabBar.frame.size.width = self.view.frame.width + 4
-//        tabBar.frame.origin.x = -2
-//        UITabBar.appearance().tintColor = UIColor(red: 255/255.0, green: 255/255.0, blue: 255/255.0, alpha: 1.0)
+        UITabBar.appearance().tintColor = PRIMARY_APP_COLOR
     }
     
 }
-
-
-//extension UIImage {
-//    
-//    class func imageWithColor(color: UIColor, size: CGSize) -> UIImage {
-//        let rect: CGRect = CGRectMake(0, 0, size.width, size.height)
-//        UIGraphicsBeginImageContextWithOptions(size, false, 0)
-//        color.setFill()
-//        UIRectFill(rect)
-//        let image: UIImage = UIGraphicsGetImageFromCurrentImageContext()
-//        UIGraphicsEndImageContext()
-//        return image
-//    }
-//    
-//}
